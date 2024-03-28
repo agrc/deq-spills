d7a2b6f962dcac2b3110cd4c0dee91c028ccf824
stdavis <stdavis@utah.gov>
2024-03-28 10:19:19 -0600

    style: fix eslint errors

--- salesforce/force-app/main/default/lwc/spills/spills.js
+++ salesforce/force-app/main/default/lwc/spills/spills.js
@@ -1,6 +1,6 @@
 import { updateRecord, getRecord } from 'lightning/uiRecordApi';
 import { LightningElement, wire, api } from 'lwc';
-import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import ID_FIELD from '@salesforce/schema/Case.Id';
 import ADDRESS from '@salesforce/schema/Case.Address_Location__c'
 import CITY from '@salesforce/schema/Case.Nearest_Town_City__c';
@@ -25,108 +25,110 @@ export default class Spills extends LightningElement {
 
   @wire(getRecord, { recordId: '$recordId', fields: [ID_FIELD, UTM_X, UTM_Y] })
   wiredRecord({ error, data}) {
-    if (error) {
-      let message = "Unknown error";
-      if (Array.isArray(error.body)) {
-        message = error.body.map((e) => e.message).join(", ");
-      } else if (typeof error.body.message === "string") {
-        message = error.body.message;
-      }
-      this.dispatchEvent(
-        new ShowToastEvent({
-          title: "Error loading contact",
-          message,
-          variant: "error",
-        }),
-      );
-    } else if (data) {
-      console.log('wc: wireRecord data', data);
-      console.log('recordId is '+ this.recordId + '/' + '$recordId');
+      if (error) {
+          let message = 'Unknown error';
+          if (Array.isArray(error.body)) {
+              message = error.body.map((e) => e.message).join(', ');
+          } else if (typeof error.body.message === 'string') {
+              message = error.body.message;
+          }
+          this.dispatchEvent(
+              new ShowToastEvent({
+                  title: 'Error loading contact',
+                  message,
+                  variant: 'error'
+              })
+          );
+      } else if (data) {
+          console.log('wc: wireRecord data', data);
+          console.log('recordId is ' + this.recordId + '/' + '$recordId');
 
-      this.utm_x = data.fields[UTM_X.fieldApiName].value;
-      this.utm_y = data.fields[UTM_Y.fieldApiName].value;
-    }
+          this.utm_x = data.fields[UTM_X.fieldApiName].value;
+          this.utm_y = data.fields[UTM_Y.fieldApiName].value;
+      }
   }
 
   constructor() {
-    console.log('wc: constructor');
+      console.log('wc: constructor', window);
 
-    super();
+      super();
   }
 
   connectedCallback() {
-    window.addEventListener('message', this.receiveMessage);
+      window.addEventListener('message', this.receiveMessage);
   }
 
   disconnectedCallback() {
-    window.removeEventListener('message', this.receiveMessage);
+      window.removeEventListener('message', this.receiveMessage);
   }
 
   receiveMessage = (event) => {
-    console.log('wc: event from iframe:', event);
+      console.log('wc: event from iframe:', event);
 
-    if (event.origin !== this.iframeSrc) {
-      console.warn(`wc: received message from unknown origin: ${event.origin}`);
-      return;
-    }
+      if (event.origin !== this.iframeSrc) {
+          console.warn(`wc: received message from unknown origin: ${event.origin}`);
+          return;
+      }
 
-    const { data } = event;
-    console.log('wc: data from iframe:', data);
-    console.log('wc: data string ' + JSON.stringify(data));
+      const { data } = event;
+      console.log('wc: data from iframe:', data);
+      console.log('wc: data string ' + JSON.stringify(data));
 
-    if (!data.UTM_X) return;
-    const utmX = Math.round(data.UTM_X);
-    const utmY = Math.round(data.UTM_Y);
+      if (!data.UTM_X) {
+          return;
+      }
+      const utmX = Math.round(data.UTM_X);
+      const utmY = Math.round(data.UTM_Y);
 
-    if (utmX === this.utm_x && utmY === this.utm_y) {
-      console.log('wc: no change in UTM_X or UTM_Y');
-      return;
-    }
+      if (utmX === this.utm_x && utmY === this.utm_y) {
+          console.log('wc: no change in UTM_X or UTM_Y');
+          return;
+      }
 
-    const fields = {
-      [ID_FIELD.fieldApiName]: this.recordId,
-      [ADDRESS.fieldApiName]: `${data.ADDRESS}, ${data.ZIP}`, // comes from widget text inputs
-      [CITY.fieldApiName]: data.CITY,
-      [COUNTY.fieldApiName]: data.COUNTY,
-      [DD_LAT.fieldApiName]: data.DD_LAT.toString(),
-      [DD_LONG.fieldApiName]: data.DD_LONG.toString(),
-      [HIGHWAY.fieldApiName]: data.HIGHWAY, // comes from widget text input
-      [INDIAN.fieldApiName]: data.INDIAN,
-      [MILEMARKER.fieldApiName]: data.MILEMARKER, // comes from widget text input
-      [OWNER_AGENCY.fieldApiName]: data.OWNER_AGENCY,
-      [UTM_X.fieldApiName]: utmX.toString(),
-      [UTM_Y.fieldApiName]: utmY.toString(),
-    }
+      const fields = {
+          [ID_FIELD.fieldApiName]: this.recordId,
+          [ADDRESS.fieldApiName]: `${data.ADDRESS}, ${data.ZIP}`, // comes from widget text inputs
+          [CITY.fieldApiName]: data.CITY,
+          [COUNTY.fieldApiName]: data.COUNTY,
+          [DD_LAT.fieldApiName]: data.DD_LAT.toString(),
+          [DD_LONG.fieldApiName]: data.DD_LONG.toString(),
+          [HIGHWAY.fieldApiName]: data.HIGHWAY, // comes from widget text input
+          [INDIAN.fieldApiName]: data.INDIAN,
+          [MILEMARKER.fieldApiName]: data.MILEMARKER, // comes from widget text input
+          [OWNER_AGENCY.fieldApiName]: data.OWNER_AGENCY,
+          [UTM_X.fieldApiName]: utmX.toString(),
+          [UTM_Y.fieldApiName]: utmY.toString()
+      }
 
-    console.log(JSON.stringify(fields));
+      console.log(JSON.stringify(fields));
 
-    updateRecord({fields}).then(() => {
-      console.log('record updating');
-      console.log('wc: record updated successfully');
-      
-    }).catch(error => {
-      console.log(JSON.stringify(error));
-      this.dispatchEvent(
-        new ShowToastEvent({
-          title: 'Error updating record',
-          message: error.body.message,
-          variant: 'error',
-        })
-      );
-    });
+      updateRecord({fields}).then(() => {
+          console.log('record updating');
+          console.log('wc: record updated successfully');
+
+      }).catch(error => {
+          console.log(JSON.stringify(error));
+          this.dispatchEvent(
+              new ShowToastEvent({
+                  title: 'Error updating record',
+                  message: error.body.message,
+                  variant: 'error'
+              })
+          );
+      });
   }
 
   renderedCallback() {
-    console.log('wc: renderedCallback');
+      console.log('wc: renderedCallback');
 
-    this.refs.iframe.addEventListener('load', () => {
-      console.log('wc: iframe loaded');
+      this.refs.iframe.addEventListener('load', () => {
+          console.log('wc: iframe loaded');
 
-      this.refs.iframe.contentWindow.postMessage({
-        UTM_X: this.utm_x,
-        UTM_Y: this.utm_y,
-        targetOrigin: window.document.location.origin,
-      }, this.iframeSrc);
-    });
+          this.refs.iframe.contentWindow.postMessage({
+              UTM_X: this.utm_x,
+              UTM_Y: this.utm_y,
+              targetOrigin: window.document.location.origin
+          }, this.iframeSrc);
+      });
   }
 }
