bdab475373f03c2790ae900e4c84afbadfa96af1
stdavis <stdavis@utah.gov>
2025-02-11 13:32:19 -0700

    feat(website): enable mouse wheel zooming when embedded
    Keep it disabled when the app is loaded on its own.
    
    Closes #130

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -27,6 +27,10 @@ const links = [
   },
 ];
 
+// get url query params
+const urlParams = new URLSearchParams(window.location.search);
+const isEmbedded = urlParams.get('embedded') === 'true';
+
 export default function App() {
   const app = useFirebaseApp();
 
@@ -47,19 +51,21 @@ export default function App() {
   return (
     <>
       <main className="flex h-screen flex-col md:gap-2">
-        <Header links={links}>
-          <div className="flex h-full grow items-center gap-3">
-            <img src="deq_logo.png" alt="DEQ Logo" className="h-12 w-12" />
-            <h2 className="font-heading text-3xl font-black text-zinc-600 sm:text-5xl dark:text-zinc-100">
-              DEQ Spills
-            </h2>
-          </div>
-        </Header>
+        {!isEmbedded ? (
+          <Header links={links}>
+            <div className="flex h-full grow items-center gap-3">
+              <img src="deq_logo.png" alt="DEQ Logo" className="h-12 w-12" />
+              <h2 className="font-heading text-3xl font-black text-zinc-600 sm:text-5xl dark:text-zinc-100">
+                DEQ Spills
+              </h2>
+            </div>
+          </Header>
+        ) : null}
         <section className="relative flex min-h-0 flex-1 overflow-x-hidden md:mr-2">
           <div className="relative flex flex-1 flex-col rounded border border-b-0 border-zinc-200 dark:border-0 dark:border-zinc-700">
             <div className="relative flex-1 overflow-hidden dark:rounded">
               <ErrorBoundary FallbackComponent={ErrorFallback}>
-                <MapContainer onClick={onClick} />
+                <MapContainer onClick={onClick} isEmbedded={isEmbedded} />
               </ErrorBoundary>
             </div>
           </div>
