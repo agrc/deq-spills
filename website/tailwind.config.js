747725e649e8b0aeed9df828b45cdde16dbf575e
stdavis <stdavis@utah.gov>
2025-02-12 10:34:04 -0700

    style(website): override default colors with DEQ logo colors

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,4 +1,5 @@
 import ugrcPreset from '@ugrc/tailwind-preset';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -10,8 +11,46 @@ export default {
   presets: [ugrcPreset],
   theme: {
     extend: {
-      fontFamily: {
-        heading: ['SourceSansPro-Black', 'Source Sans Pro', 'Arial', 'Helvetica', 'sans-serif'],
+      colors: {
+        primary: {
+          50: '#E0F6FF',
+          100: '#BDEBFF',
+          200: '#7AD7FF',
+          300: '#3DC5FF',
+          400: '#00AFFA',
+          500: '#0080B7',
+          600: '#006894',
+          700: '#004F70',
+          800: '#003247',
+          900: '#001924',
+          950: '#000E14',
+        },
+        secondary: {
+          50: '#E2F8FE',
+          100: '#C9F2FD',
+          200: '#93E6FB',
+          300: '#5DD9F8',
+          400: '#27CDF6',
+          500: '#09B0DA',
+          600: '#078FB0',
+          700: '#056B84',
+          800: '#044758',
+          900: '#02242C',
+          950: '#011014',
+        },
+        accent: {
+          50: '#F7FBEF',
+          100: '#EEF6DF',
+          200: '#DEEDBF',
+          300: '#CDE49F',
+          400: '#BDDB7F',
+          500: '#ADD361',
+          600: '#91BF35',
+          700: '#6D9028',
+          800: '#49601B',
+          900: '#24300D',
+          950: '#121807',
+        },
       },
     },
   },
