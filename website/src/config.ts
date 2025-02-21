a588083e82c4c64b70ec74ca04a89d77d59aeb4b
stdavis <stdavis@utah.gov>
2025-02-21 12:26:33 -0700

    feat(website): add legend and labels for land ownership
    Ref: #128

--- website/src/config.ts
+++ website/src/config.ts
@@ -3,6 +3,11 @@ const config = {
   WEB_MERCATOR_WKID: 3857,
   MARKER_FILL_COLOR: [234, 202, 0, 0.5] as [number, number, number, number],
   MARKER_OUTLINE_COLOR: [77, 42, 84, 1] as [number, number, number, number],
+  URL: {
+    landownership: 'https://gis.trustlands.utah.gov/mapping/rest/services/Land_Ownership/FeatureServer/0',
+    waterSystems:
+      'https://services2.arcgis.com/NnxP4LZ3zX8wWmP9/ArcGIS/rest/services/Utah_DDW_Public_Water_System_Sources/FeatureServer/0',
+  },
 };
 
 export default config;
