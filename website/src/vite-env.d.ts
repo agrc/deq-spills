363cfb53aac07c9bffa1708f17ba49db379bb38b
stdavis <stdavis@utah.gov>
2025-03-10 13:00:49 -0600

    feat(website): implement define location by address or route/milepost
    Ref #136

--- website/src/vite-env.d.ts
+++ website/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_DISCOVER: string;
+  readonly VITE_WEB_API: string;
+  readonly VITE_FIREBASE_CONFIG: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
