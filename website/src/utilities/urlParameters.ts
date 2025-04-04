a75a60334d1419ef0130f76f6a9b9951f374ac03
stdavis <stdavis@utah.gov>
2025-04-04 16:10:59 -0600

    feat(website): add spill number and field aliases to standalone sidebar

--- website/src/utilities/urlParameters.ts
+++ website/src/utilities/urlParameters.ts
@@ -1,4 +1,4 @@
-import { blankState, booleanKeys, DataContext, numericKeys } from '../contexts/DataProvider';
+import { blankState, booleanKeys, type DataContextType, numericKeys } from '../contexts/DataProvider';
 
 export function getUrlParam(param: string): number | string | boolean | null {
   const params = new URLSearchParams(window.location.search);
@@ -24,12 +24,12 @@ export function getIsReadOnly(): boolean {
   return getUrlParam('readonly') === 'true';
 }
 
-export function getData(): DataContext['data'] {
+export function getData(): DataContextType['data'] {
   const data: { [key: string]: number | string | boolean | null } = {};
 
   for (const key in blankState) {
     data[key] = getUrlParam(key);
   }
 
-  return data as DataContext['data'];
+  return data as DataContextType['data'];
 }
