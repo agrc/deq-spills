56e2eed79875c176861eb4a1351092a85a0ab8c5
stdavis <stdavis@utah.gov>
2025-04-04 12:32:44 -0600

    chore(website): switch to @ugrc/esri-theme-toggle

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -1,10 +1,14 @@
+import esriConfig from '@arcgis/core/config';
+import initializeTheme from '@ugrc/esri-theme-toggle';
 import { FirebaseAnalyticsProvider, FirebaseAppProvider } from '@ugrc/utah-design-system';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import EsriCss from './components/EsriCss';
 import { DataProvider } from './contexts/DataProvider';
 import './index.css';
+
+esriConfig.assetsPath = '/assets';
+initializeTheme();
 
 let firebaseConfig = {
   apiKey: '',
@@ -22,7 +26,6 @@ if (import.meta.env.VITE_FIREBASE_CONFIG) {
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <EsriCss />
     <FirebaseAppProvider config={firebaseConfig}>
       <FirebaseAnalyticsProvider>
         <DataProvider>
