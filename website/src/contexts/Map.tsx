067db7c295dcfcd0aea95a7e71d4957082a816cb
stdavis <stdavis@utah.gov>
2026-01-26 06:52:53 -0700

    fix(website): handle more flow paths use cases and environments
    Ref #129

--- website/src/contexts/Map.tsx
+++ website/src/contexts/Map.tsx
@@ -3,11 +3,18 @@ import { createContext, useState } from 'react';
 type MapContextType = {
   setMapView: (view: __esri.MapView) => void;
   mapView: __esri.MapView | null;
+  setFlowPathFeatureLayer: (layer: __esri.FeatureLayer) => void;
+  flowPathFeatureLayer: __esri.FeatureLayer | null;
 };
 export const MapContext = createContext<MapContextType | null>(null);
 
 export const MapProvider = ({ children }: { children: React.ReactNode }) => {
   const [mapView, setMapView] = useState<__esri.MapView | null>(null);
+  const [flowPathFeatureLayer, setFlowPathFeatureLayer] = useState<__esri.FeatureLayer | null>(null);
 
-  return <MapContext.Provider value={{ setMapView, mapView }}>{children}</MapContext.Provider>;
+  return (
+    <MapContext.Provider value={{ setMapView, mapView, setFlowPathFeatureLayer, flowPathFeatureLayer }}>
+      {children}
+    </MapContext.Provider>
+  );
 };
